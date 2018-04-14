1f1b243d4d928076fa82da8008b6431ad539b592
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-14 21:57:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ function getFirstSelector(selector) {
  }
  
 function increaseRankBy(n) {
-  var ranks = document.querySelectorAll('ul.ranked-list li');
+  var ranks = document.querySelectorAll('ul.ranked-list li')
   for (var i = 0; i < ranks.length; i++) {
     ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
   }
 }
 
 function deepestChild() {
-  let grandNode = document.querySelector('#grand-node');
-  let deepestNode = grandNode.children[0];
+  let grandNode = document.querySelector('#grand-node')
+  let deepestNode = grandNode.children[0]
   for (let i = 0; deepestNode.children[i]; i) {
     deepestNode = deepestNode.children[0]
   }
-  return deepestNode;
+  return deepestNode
 }
