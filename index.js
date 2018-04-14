4ef65678913d65e4ff57a6accf663e2e992d5610
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-14 21:47:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function getFirstSelector(selector) {
    return document.querySelector('#nested, .target')
  }
  
-function deepestChild() {
-  return document.querySelector('#grand-node')
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
 }
